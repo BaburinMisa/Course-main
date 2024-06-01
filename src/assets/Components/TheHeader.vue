d0412ae74ad4e971bea9ad61b02c99f30e154a92
Misha <phonelager2005@mail.ru>
2024-06-01 13:18:35 +0700

    git commit --amend

--- src/assets/Components/TheHeader.vue
+++ src/assets/Components/TheHeader.vue
@@ -31,35 +31,46 @@ header {
   background: #000000;
   color: white;
   font-family: "Roboto Light";
+  border-bottom: 1px solid white;
 }
+
 .header-items {
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
 }
+
 .header-list-1 {
   font-family: "Roboto", sans-serif;
   display: flex;
   flex-direction: row;
   align-items: center;
 }
+
 .header-list-1-ul {
   list-style-type: none;
   margin-right: 1em;
   font-size: 1.1em;
   margin-top: 0.7em;
-
+  transition: border-bottom 0.3s ease;
 }
+
+.header-list-1-ul:hover {
+  border-bottom: 2px solid #ffffff;
+}
+
 .header-list-1 a {
   text-decoration: none;
   color: #fff;
 }
+
 .header-list-2 {
   display: flex;
   flex-direction: row;
   align-items: center;
 }
+
 .header-list-2 li {
   list-style-type: none;
   margin-right: 0.5em;
@@ -71,22 +82,27 @@ header {
     height: auto;
     padding: 1em 0;
   }
+
   .header-items {
     flex-direction: column;
   }
+
   .header-list-1 {
     flex-direction: column;
     margin-top: 1em;
     align-items: center;
   }
+
   .header-list-1-ul {
     margin-right: 0;
     margin-bottom: 1em;
   }
+
   .header-list-2 {
     margin-top: 1em;
     justify-content: center;
   }
+
   .header-list-2 li {
     margin-right: 1em;
   }
